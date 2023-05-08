d74f9ee3963a55b28a87b6e815d6b47c35847f28
kevenwallace <kevenwallace_@hotmail.com>
2023-05-08 11:09:25 -0300

    curva de aprendizado

--- inicio3.py
+++ inicio3.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -34,3 +36,28 @@ print(f"resultado foi de: {(resultado * 100):.2f}%")
 previsoea_de_base = np.ones(len(test_x))
 resultado = accuracy_score(test_y, previsoea_de_base)
 print(f"resultado das previsoes de base foi de: {(resultado * 100):.2f}%")
+
+#-curva_de_aprendizagem
+sns.scatterplot(x='expected_hours', y='price',hue=test_y, data=test_x)
+
+x_min = test_x.expected_hours.min()
+x_max = test_x.expected_hours.max()
+
+y_min = test_x.price.min()
+y_max = test_x.price.max()
+
+print(x_min, x_max, y_min, y_max)
+
+pixel = 100
+eixo_x = np.arange(x_min, x_max, (x_max - x_min) / pixel)
+eixo_y = np.arange(y_min, y_max, (y_max - y_min) / pixel)
+
+xx, yy = np.meshgrid(eixo_x, eixo_y)
+
+pontos = np.c_[xx.ravel(), yy.ravel()]
+z = modelo.predict(pontos)
+z = z.reshape(xx.shape)
+
+plt.contourf(xx, yy, z, alpha=0.3)
+plt.scatter(test_x.expected_hours, test_x.price, c=test_y, s=1)
+
